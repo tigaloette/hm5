f8e9cf207ea7e1a9434d473338d709fc3b36b9dc
tigaloette <35683028+tigaloette@users.noreply.github.com>
2018-02-19 21:51:10 -0800

    Update water-arrays.js

--- water-arrays.js
+++ water-arrays.js
@@ -5,19 +5,27 @@ var ySpeed = [];
 
 function setup() {
   createCanvas(400, 400);
+
+  for (var i = 0; i < 10; i = i + 1) {
+    x[i] = width / 2;
+    y[i] = height / 2;
+    xSpeed[i] = random(0.5,1);
+    ySpeed[i] = random(1,3);
+  }
 }
-  
+
 function draw() {
   background(0);
   noStroke();
-
-  rect(0, 20, 230, 20);
-  for (var i = 0; i < 10; i = i + 1){
-  ellipse(x[i], y[i], 10);
-  
-  y[i] = y[i] + ySpeed[i];
-}  
-  if (y > height) {
-    y = 30;
+rect(0, 200, width/2, 20)
+  for (var i = 0; i < 10; i = i + 1) {
+   
+    ellipse(x[i], y[i],10);
+    x[i] = x[i] + xSpeed[i] + i;
+    y[i] = y[i] + ySpeed[i] + i;
+    
+if (i > 10) {
+    y = height / 2;
+    }
   }
 }
